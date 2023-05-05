bfba083adaf0a4b54274b2892a2636b04db5f931
GABRIEL DOS REIS REDORAT <22.01583-3@maua.br>
2023-05-05 09:09:23 -0300

    Exercicio aula 9
    Signed-off-by: GABRIEL DOS REIS REDORAT <22.01583-3@maua.br>

--- methods.py
+++ methods.py
@@ -5,3 +5,21 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def soma(valor_1, valor_2):
+    soma = valor_1 + valor_2
+    return soma 
+
+def subtracao(valor_1, valor_2):
+    subtracao = valor_1 - valor_2
+    return subtracao
+
+def multiplicacao(valor_1, valor_2):
+    multiplicacao = valor_1 * valor_2
+    return multiplicacao
+
+def divisao(valor_1, valor_2):
+    divisao = valor_1 / valor_2
+    return divisao
+
+
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,48 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_soma():
+    #given two values 
+    valor_1 = 5 
+    valor_2 = 3
+
+    # when we calculate the soma
+    output = methods.soma(valor_1, valor_2)
+    
+    # then the perimeter should be 14
+    assert output == 8
+
+
+def test_subtracao():
+    #given two values 
+    valor_1 = 10
+    valor_2 = 4
+
+    # when we calculate the subtracao
+    output = methods.subtracao(valor_1, valor_2)
+    
+    # then the subtracao should be 6
+    assert output == 6
+
+def test_multiplicacao():
+    #given two values 
+    valor_1 = 5
+    valor_2 = 7
+
+    # when we calculate the multiplicacao
+    output = methods.multiplicacao(valor_1, valor_2)
+    
+    # then the multiplicacao should be 35
+    assert output == 35
+
+def test_divisao():
+    #given two values 
+    valor_1 = 20
+    valor_2 = 5
+
+    # when we calculate the divisao
+    output = methods.divisao(valor_1, valor_2)
+    
+    # then the divisao should be 4
+    assert output == 4
